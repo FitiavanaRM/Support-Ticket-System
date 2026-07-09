a02acef3e5d8b94a94a757aec45c54f52d513abe
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-07-09 16:40:00 +0300

    ajouter StateException, base des erreurs de machine a etats

---
+++ src/Exceptions/StateException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+// erreur liée à la machine (tickets)
+
+class StateException extends BusinessException
+{
+    public function httpStatusCode(): int
+    {
+        return 422;
+    }
+
+    protected function defaultMessage(): string
+    {
+        return "L'operation demandee n'est pas compatible avec l'etat actuel du ticket.";
+    }
+}
